018d25690f1f9685420e2e321b84e99b3279c7c4
Esteban Soto <esoto@pernix-solutions.com>
2013-10-24 12:50:42 -0600

    post must receive attributes

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -4,5 +4,13 @@ STester.description Post do
       post = Post.new
       post.should_not be_equal nil
     end
+
+    STester.it "accepts attributes as title and post_body" do
+      title = "this is the title"
+      post_body = "this is the post body"
+      post = Post.new(title: title, post_body: post_body)
+      post.title.should be_equal title
+      post.post_body.should be_equal post_body
+    end
   end
 end
