ee36103f5b7a357bd93707ef22444bd9b858b17b
Esteban Soto <esoto@pernix-solutions.com>
2013-10-25 17:27:27 -0600

    assign id to the post after create

--- spec/csv_manager_spec.rb
+++ spec/csv_manager_spec.rb
@@ -13,5 +13,12 @@ STester.description CSVManager do
       posts = Post.all
       posts.first.title.should be_equal "This is a new post"
     end
+
+    STester.it "assigns a new id" do
+      new_post = Post.new(title: "This is a new post", post_body: "This is the body")
+      Post.create new_post
+      posts = Post.all
+      posts.first.id.should_not be_nil
+    end
   end
 end
