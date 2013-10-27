6572bcfbb482b1dc2d28eaca33f10ed7987be16f
Esteban Soto <esoto@pernix-solutions.com>
2013-10-26 18:52:44 -0600

    small refactor on assign id and return all method

--- lib/csv_manager.rb
+++ lib/csv_manager.rb
@@ -4,13 +4,8 @@ class CSVManager
   attr_accessor :id
 
   def self.all
-    posts = []
-    CSV.read("lib/posts.csv").each do |post_attr|
-      post  = Post.new(title: post_attr[1], post_body: post_attr[2])
-      post.id = post_attr[0]
-      posts << post
-    end
-    posts
+    posts = read_post_file
+    posts.map{|post_attr| set_up_posts(post_attr) }
   end
 
   def self.create post
@@ -20,12 +15,18 @@ class CSVManager
     end
   end
 
+  private
+
   def assign_id
-    if File.exist?("lib/posts.csv")
-      id = CSV.read("lib/posts.csv").last[0].to_i + 1
-      self.id = id
-    else
-      self.id = 1
-    end
+    self.id = File.exist?("lib/posts.csv") ? (read_post_file.last[0].to_i + 1) : 1
+  end
+
+  def read_post_file
+    CSV.read("lib/posts.csv")
+  end
+
+  def set_up_posts post_attr
+    post  = Post.new(title: post_attr[1], post_body: post_attr[2])
+    post.id = post_attr[0]
   end
 end
