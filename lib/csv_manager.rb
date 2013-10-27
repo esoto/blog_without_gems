fb4c7d7aa0908e8b0be5394bda83748ce1ee78d3
Esteban Soto <esoto@pernix-solutions.com>
2013-10-26 18:33:41 -0600

    return id on all

--- lib/csv_manager.rb
+++ lib/csv_manager.rb
@@ -1,7 +1,16 @@
 require 'csv'
 
 class CSVManager
+  attr_accessor :id
+
   def self.all
+    posts = []
+    CSV.read("lib/posts.csv").each do |post_attr|
+      post  = Post.new(title: post_attr[1], post_body: post_attr[2])
+      post.id = post_attr[0]
+      posts << post
+    end
+    posts
   end
 
   def self.create post
